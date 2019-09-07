ed8bc6eaad0552e8b94b443f78c8236db2f25254
Brennan Neilson <bvneilson@gmail.com>
2019-09-07 10:57:09 -0600

    First event listener

--- js/index.js
+++ js/index.js
@@ -1,1 +1,11 @@
 // Your code goes here
+const logo = document.querySelector('.logo-heading');
+console.log(logo);
+logo.addEventListener("click", (event) => {
+  if (logo.style.color === 'blue') {
+    logo.style.color = 'black';
+  } else {
+      logo.style.color = 'blue';
+  }
+  console.log('Logo clicked!');
+});
